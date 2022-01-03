cffdcb29364cc78bb98cdd05b7ec21e5a023b7d6
ardra <ardra@DESKTOP-PNM8KQP>
2022-01-03 19:21:40 +0530

    First Commit Uploading All Files

--- SeleniumJavaFramework/src/test/java/com/Yatra/testcases/HomePageTest.java
+++ SeleniumJavaFramework/src/test/java/com/Yatra/testcases/HomePageTest.java
@@ -1,5 +1,20 @@
 package com.Yatra.testcases;
 
-public class HomePageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import com.Yatra.base.BaseClass;
+import com.Yatra.pages.BookingPage;
+import com.Yatra.pages.HomePage;
+
+public class HomePageTest extends BaseClass {
+	HomePage homePage;
+	BookingPage bookingPage;
+@Test
+public void VerifyUrl() {
+	bookingPage=homePage.search();
+	String expectedURL ="https://www.yatra.com/";
+	String actualURL = driver.getCurrentUrl();
+	Assert.assertEquals(actualURL, expectedURL);
+	}
 }
